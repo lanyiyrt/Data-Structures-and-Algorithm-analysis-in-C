0bd9d63e112807016bdeb5dd40d0e2c608e75be0
Yuruitao <lanyiyrt@users.noreply.github.com>
2017-10-26 16:22:56 +0800

    Update 3.1.c
    打印单链表中的所有元素

--- chapter3/3.1.c
+++ chapter3/3.1.c
@@ -1,1 +1,12 @@
-
+void printf(List L){
+  List P;
+  P = L->Next;
+  if(P == NULL){
+    printf("No data!");
+    return;
+  }
+  while(P != NULL){
+    printf("%d ", P->Element);
+    P = P->Next;
+  }
+}
